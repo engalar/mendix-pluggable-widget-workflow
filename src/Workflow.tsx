7f17b1abb5ad8d18f62c35d320a428af495b2c5e
engalar <engalar@gmail.com>
2022-05-23 17:22:55 +0800

    demo

--- src/Workflow.tsx
+++ src/Workflow.tsx
@@ -1,18 +1,58 @@
-import { createElement, useMemo } from "react";
+import { createElement } from "react";
 import { WorkflowContainerProps } from "../typings/WorkflowProps";
-import { ValueStatus } from "mendix";
+import ReactFlow, {
+    addEdge,
+    MiniMap,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+} from 'react-flow-renderer';
 
-export default function (props: WorkflowContainerProps) {
-    console.log(eval("{a:1}"));
-    const value = useMemo(() => {
-        if (props.attribute && props.attribute.status === ValueStatus.Available) {
-            return props.attribute.value;
-        }
-    }, [props.attribute]);
+import { nodes as initialNodes, edges as initialEdges } from './initial-elemen';
+
+const onInit = (reactFlowInstance: any) => console.log('flow loaded:', reactFlowInstance);
+
+const OverviewFlow = () => {
+    const [nodes, _setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    const onConnect = (params: any) => setEdges((eds) => addEdge(params, eds));
 
     return (
-        <div>
-            hello {props.sampleText} and your value is {value}
-        </div>
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={onInit}
+            fitView
+            attributionPosition="top-right"
+        >
+            <MiniMap
+                nodeStrokeColor={(n: any) => {
+                    if (n.style?.background) return n.style.background;
+                    if (n.type === 'input') return '#0041d0';
+                    if (n.type === 'output') return '#ff0072';
+                    if (n.type === 'default') return '#1a192b';
+
+                    return '#eee';
+                }}
+                nodeColor={(n: any) => {
+                    if (n.style?.background) return n.style.background;
+
+                    return '#fff';
+                }}
+                nodeBorderRadius={2}
+            />
+            <Controls />
+            <Background color="#aaa" gap={16} />
+        </ReactFlow>
+    );
+};
+export default function (props: WorkflowContainerProps) {
+    console.log(props);
+    return (
+        <OverviewFlow />
     );
 }
--- typings/WorkflowProps.d.ts
+++ typings/WorkflowProps.d.ts
@@ -1,12 +1,12 @@
 /**
- * This file was generated from Graph.xml
+ * This file was generated from Workflow.xml
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix UI Content Team
  */
 import { CSSProperties } from "react";
 import { EditableValue, ListValue, ListAttributeValue } from "mendix";
 
-export interface GraphContainerProps {
+export interface WorkflowContainerProps {
     name: string;
     class: string;
     style?: CSSProperties;
@@ -17,7 +17,7 @@ export interface GraphContainerProps {
     att2?: ListAttributeValue<string>;
 }
 
-export interface GraphPreviewProps {
+export interface WorkflowPreviewProps {
     className: string;
     style: string;
     styleObject?: CSSProperties;
