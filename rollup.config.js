790d4e1bcee881271b11c6b2deb8459235c17964
engalar <engalar@gmail.com>
2022-05-29 12:23:34 +0800

    chore: upgrade studio 9.12.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,9 @@
 import commonjs from '@rollup/plugin-commonjs';
-// import alias from "@rollup/plugin-alias";
-// import { join } from "path";
 
 export default args => {
   const result = args.configDefaultConfig;
   const [jsConfig, mJsConfig] = result;
   [jsConfig, mJsConfig].forEach(config => {
-    // config.external = [/^mendix($|\/)/, /^react($|\/)/, /^react-dom($|\/)/, /^big.js$/];
-    // config.plugins[6] = alias({
-    //   entries: {
-    //     "react-hot-loader/root": join(__dirname, "node_modules/@mendix/pluggable-widgets-tools/configs/hot"),
-    //   }
-    // });
-
     // copy from mx
     config.plugins[10] = commonjs({
       extensions: ['.js', '.jsx', '.tsx', '.ts'],
